6392365702d06974019ca366f99f17ebfdf90f61
Vlad Basin <vladbasin@gmail.com>
2020-09-08 12:49:31 +0300

    Updating version and index.

--- src/index.ts
+++ src/index.ts
@@ -13,3 +13,5 @@ export * from "./LoaderProvider/Hooks/useLoader";
 export * from "./LoaderProvider/Models/LoaderContext";
 export * from "./LoaderProvider/Types/LoaderContextType";
 export * from "./LoaderProvider/Types/LoaderTaskType";
+export * from "./TabMenu/TabMenu";
+export * from "./TabMenu/Types/TabMenuItemType";
